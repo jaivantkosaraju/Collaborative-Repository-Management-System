4a680f473b6be4b44437c3b0dd72fd82412890a4
Jaivant <your_email@example.com>
2025-06-10 21:52:19 +0530

    first commit

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('my_db', 'root', 'password', {
+const sequelize = new Sequelize('my_db', 'root', 'Jaithu@123', {
   host: 'localhost',
   dialect: 'mysql',
   logging:true
